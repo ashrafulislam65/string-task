adc0bf58c87abe27ddffd5a72b515f12f8d119c1
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-05 13:06:15 +0600

    added string contains all the vowels or not

---
+++ contain-vowles.js
@@ -0,0 +1,9 @@
+// Check whether a string contains all the vowels a, e, i, o, u
+let str = 'aeiou';
+if(str.includes('a')&& str.includes('e')&& str.includes('i')&& str.includes('o')&& str.includes('u'))
+{
+    console.log('contains all the vowels');
+}
+else{
+    console.log('not contains all the vowels');
+}
